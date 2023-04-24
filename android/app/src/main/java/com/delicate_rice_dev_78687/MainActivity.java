3f4fcdd4a6d5096ef162588f69fb1d5f449a88c1
Crowdbotics <engineering@crowdbotics.com>
2023-04-24 11:14:06 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/delicate_rice_dev_78687/MainActivity.java
@@ -0,0 +1,15 @@
+package com.delicate_rice_dev_78687;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "delicate_rice_dev_78687";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delicate-rice-dev-78687",
+  url: "https://delicate-rice-dev-78687.botics.co",
+  api: "https://delicate-rice-dev-78687.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
